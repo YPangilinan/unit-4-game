5718898b864742639c4654b9b1a2a0a18f3d5fe4
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-04-26 15:25:45 -0700

    pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+//assign variables
+
+//counter variables
+
+//for loop for cost options for flowers
+
+//each iteration will create variable image for flower
+
+//each flower will be given a class ".flower-image"
+
+//each flower has a src link
+
+//var imageFlower will be given an attribute (data-flowerValue) and will be set equal to array value
+
+//each image with all class and attribute will get added to page using jquery dom manipulation
+
+//on click even for each of the flowers on the page
+
+    //value requires extractoin of value from data attr
+    //use ($this) keyword specifies to extract the flower value from the clicked flower
+    //use .attr("data-flowerValue") to grab value out of the attribute
+    //need to convert string to integer before adding to counter (use parseInt)
+
+
+//add flowerValue to counter global variable
+//each click gets added to the global counter
+
+//win-lose logic
+
+//reset function
+
+//adding to the dom
